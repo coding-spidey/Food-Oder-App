864e68e3fc3197616c812e0375f90c267d2d20fb
coding-spidey <rishabhgoswami938@gmail.com>
2021-05-25 13:25:31 +0530

    add database

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,52 +1,53 @@
+import { useEffect, useState } from "react";
+
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const DUMMY_MEALS = [
-    {
-      id: "m1",
-      name: "Shahi Paneer",
-      description: "Made from real paneer",
-      price: 40,
-    },
-    {
-      id: "m2",
-      name: "Rajma",
-      description: "Nutritious yet delicious",
-      price: 35,
-    },
-    {
-      id: "m3",
-      name: "Kadhi",
-      description: "Every Yellow liquid is not disgusting",
-      price: 35,
-    },
-    {
-      id: "m4",
-      name: "Roti",
-      description: "Bread",
-      price: 5,
-    },
-  ];
-  const MealsList = DUMMY_MEALS.map((meal) => {
-    return (
-      <MealItem
-        key={meal.id}
-        id={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
-    );
-  });
+  const [meals, setMeals] = useState([]);
+  console.log(process.env.REACT_APP_URL);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(process.env.REACT_APP_URL);
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{MealsList}</ul>
+        <ul>{mealsList}</ul>
       </Card>
     </section>
   );
 };
+
 export default AvailableMeals;
